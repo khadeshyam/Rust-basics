db422c87ab2067d73f1dfcaee77f82bbf4383e59
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-02-12 15:26:34 +0530

    learn variables in rust

---
+++ src/vars.rs
@@ -0,0 +1,19 @@
+pub fn run(){
+      let name = "Brad";
+      
+      let mut age = 37;
+      println!("My name is {} and I am {}",name,age);
+
+      age = 38;
+      println!("My name is {} and I am {}",name,age);
+
+      //const Defination
+      const ID : i32 = 001;
+      println!("ID: {} ",ID);
+
+      //assign multiple vars
+      let (my_name,my_age) = ("Shyam",19);
+      println!("{} is {}",my_name,my_age);
+
+
+}
